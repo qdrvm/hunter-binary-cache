9ffb3d14b591fb0b4b0b04ec36e91c0cde076296
warchant <warchant@users.noreply.github.com>
2019-07-17 10:24:06 +0000

    Uploading cache info
    Travis:
      TRAVIS_BRANCH: master
      TRAVIS_BUILD_ID: 559891030
      TRAVIS_BUILD_NUMBER: 1943
      TRAVIS_JOB_ID: 559891033
      TRAVIS_JOB_NUMBER: 1943.3
      TRAVIS_OS_NAME: linux
      TRAVIS_REPO_SLUG: soramitsu/kagome
    
      Job URL: https://travis-ci.org/soramitsu/kagome/jobs/559891033

---
+++ fd879f9/GTest/1.8.0-hunter-p11/76c6aec/2f703df/args.cmake
@@ -0,0 +1,3 @@
+set("HUNTER_INSTALL_LICENSE_FILES" "googletest/LICENSE" CACHE INTERNAL "")
+set("gtest_force_shared_crt" "ON" CACHE INTERNAL "")
+set("gtest_disable_pthreads" "OFF" CACHE INTERNAL "")
