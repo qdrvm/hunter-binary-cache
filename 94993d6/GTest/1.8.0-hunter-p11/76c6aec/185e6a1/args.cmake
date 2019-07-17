7fa73d90fc8b41264794abb0ee910f870ad359f2
sorabot <sorabot@users.noreply.github.com>
2019-07-17 10:27:33 +0000

    Uploading cache info

---
+++ 94993d6/GTest/1.8.0-hunter-p11/76c6aec/185e6a1/args.cmake
@@ -0,0 +1,3 @@
+set("HUNTER_INSTALL_LICENSE_FILES" "googletest/LICENSE" CACHE INTERNAL "")
+set("gtest_force_shared_crt" "OFF" CACHE INTERNAL "")
+set("gtest_disable_pthreads" "OFF" CACHE INTERNAL "")
