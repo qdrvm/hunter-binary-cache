94d1044decfe3de5c7213ab3ede71774fef29236
xDimon <xDimon@users.noreply.github.com>
2021-03-19 08:39:20 +0300

    Uploading cache info

---
+++ 252be92/GTest/1.8.0-hunter-p11/76c6aec/5a53a1e/args.cmake
@@ -0,0 +1,3 @@
+set("HUNTER_INSTALL_LICENSE_FILES" "googletest/LICENSE" CACHE INTERNAL "")
+set("gtest_force_shared_crt" "TRUE" CACHE INTERNAL "")
+set("CMAKE_CXX_FLAGS" "-Wno-deprecated-copy" CACHE INTERNAL "")
