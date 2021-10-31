83c8978facd0b80338eb632d58257612ca90ca40
xDimon <xDimon@users.noreply.github.com>
2021-10-31 22:41:48 +0300

    Uploading cache info

---
+++ 0e1b04e/GTest/7b100bb68db8df1060e178c495f3cbe941c9b058/7b100bb/3916655/args.cmake
@@ -0,0 +1,6 @@
+set("HUNTER_INSTALL_LICENSE_FILES" "googletest/LICENSE" CACHE INTERNAL "")
+set("gtest_force_shared_crt" "TRUE" CACHE INTERNAL "")
+set("CMAKE_CXX_FLAGS" "-Wno-deprecated-copy" CACHE INTERNAL "")
+set("HUNTER_INSTALL_LICENSE_FILES" "LICENSE" CACHE INTERNAL "")
+set("CMAKE_CXX_FLAGS" "-Wno-unknown-warning-option" CACHE INTERNAL "")
+set("gtest_force_shared_crt" "TRUE" CACHE INTERNAL "")
